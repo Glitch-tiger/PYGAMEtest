8dc3c7359345611006dc0c1ca1a51f67c4acfaae
Glitch-tiger <lofa45@gmail.com>
2025-04-08 15:06:17 +1100

    Lesson2

---
+++ main.py
@@ -0,0 +1,33 @@
+import pygame
+import sys
+from pygame.locals import QUIT
+
+def main():
+    # Инициализация Pygame
+    pygame.init()
+    
+    # Установка размеров окна и создание поверхности для отображения
+    DISPLAYSURF = pygame.display.set_mode((400, 300))
+    
+    # Установка заголовка окна
+    pygame.display.set_caption('КРУТАЯ ПРОГРАММА')
+    
+    # Главный игровой цикл
+    while True:
+        # Обработка событий
+        for event in pygame.event.get():
+            if event.type == QUIT:  # Проверка на событие выхода
+                pygame.quit()  # Завершение Pygame
+                sys.exit()     # Выход из программы
+
+        # Сюда вставляем код, если хотим отрисовать объект до обновления экрана
+        
+        # Обновление экрана с текущими отрисованными объектами
+        pygame.display.update()
+        
+        # Сюда вставляем код, если хотим отрисовать объект после обновления экрана
+
+# Запуск функции main при выполнении скрипта
+if __name__ == "__main__":
+    main()
+    
